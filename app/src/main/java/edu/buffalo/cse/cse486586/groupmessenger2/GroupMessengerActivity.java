248dbf9db50e99b286bb589378f51f6c7608a05a
a_m_a_n <amanbhayana@yahoo.in>
2019-03-04 19:53:19 -0500

    Initial commit

---
+++ app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerActivity.java
@@ -0,0 +1,67 @@
+package edu.buffalo.cse.cse486586.groupmessenger2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * GroupMessengerActivity is the main Activity for the assignment.
+ * 
+ * @author stevko
+ *
+ */
+public class GroupMessengerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_messenger);
+
+        /*
+         * TODO: Use the TextView to display your messages. Though there is no grading component
+         * on how you display the messages, if you implement it, it'll make your debugging easier.
+         */
+        TextView tv = (TextView) findViewById(R.id.textView1);
+        tv.setMovementMethod(new ScrollingMovementMethod());
+        
+        /*
+         * Registers OnPTestClickListener for "button1" in the layout, which is the "PTest" button.
+         * OnPTestClickListener demonstrates how to access a ContentProvider.
+         */
+        findViewById(R.id.button1).setOnClickListener(
+                new OnPTestClickListener(tv, getContentResolver()));
+        
+        /*
+         * TODO: You need to register and implement an OnClickListener for the "Send" button.
+         * In your implementation you need to get the message from the input box (EditText)
+         * and send it to other AVDs.
+         */
+
+
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button send = (Button) findViewById(R.id.button4);
+                String message = send.getText().toString();
+                send.setText("");
+
+
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_group_messenger, menu);
+        return true;
+    }
+}
---
+++ app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerProvider.java
@@ -0,0 +1,155 @@
+package edu.buffalo.cse.cse486586.groupmessenger2;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * GroupMessengerProvider is a key-value table. Once again, please note that we do not implement
+ * full support for SQL as a usual ContentProvider does. We re-purpose ContentProvider's interface
+ * to use it as a key-value table.
+ * 
+ * Please read:
+ * 
+ * http://developer.android.com/guide/topics/providers/content-providers.html
+ * http://developer.android.com/reference/android/content/ContentProvider.html
+ * 
+ * before you start to get yourself familiarized with ContentProvider.
+ * 
+ * There are two methods you need to implement---insert() and query(). Others are optional and
+ * will not be tested.
+ * 
+ * @author stevko
+ *
+ */
+public class GroupMessengerProvider extends ContentProvider {
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // You do not need to implement this.
+        return 0;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        // You do not need to implement this.
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        /*
+         * TODO: You need to implement this method. Note that values will have two columns (a key
+         * column and a value column) and one row that contains the actual (key, value) pair to be
+         * inserted.
+         * 
+         * For actual storage, you can use any option. If you know how to use SQL, then you can use
+         * SQLite. But this is not a requirement. You can use other storage options, such as the
+         * internal storage option that we used in PA1. If you want to use that option, please
+         * take a look at the code for PA1.
+         */
+        Log.i("Insert-", "Calling the insertInInternalStorage function");
+        uri= insertInInternalStorage(uri, values, getContext());
+        Log.v("insert", values.toString());
+        return uri;
+    }
+    private Uri insertInInternalStorage(Uri uri, ContentValues contentValues, Context context)
+    {
+        FileOutputStream outputStream = null;
+        try
+        {
+            String fileName = contentValues.getAsString("key");
+            String value = contentValues.getAsString("value");
+            if( (fileName != null) && (value != null)) {
+                outputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+                outputStream.write(value.getBytes());
+                outputStream.flush();
+                outputStream.close();
+            }
+            else return null;
+        }
+        catch(IOException io)
+        {
+            io.printStackTrace();
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally {
+//            outputStream.close();
+        }
+
+        return uri;
+    }
+
+    @Override
+    public boolean onCreate() {
+        // If you need to perform any one-time initialization task, please do it here.
+        return false;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        // You do not need to implement this.
+        return 0;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        /*
+         * TODO: You need to implement this method. Note that you need to return a Cursor object
+         * with the right format. If the formatting is not correct, then it is not going to work.
+         *
+         * If you use SQLite, whatever is returned from SQLite is a Cursor object. However, you
+         * still need to be careful because the formatting might still be incorrect.
+         *
+         * If you use a file storage option, then it is your job to build a Cursor * object. I
+         * recommend building a MatrixCursor described at:
+         * http://developer.android.com/reference/android/database/MatrixCursor.html
+         */
+        Log.i("Query-","Calling the getQueryResult()");
+        Cursor returnResult = getQueryResult(uri, selection, getContext());
+        Log.v("query", selection);
+        return returnResult;
+    }
+    private  Cursor getQueryResult(Uri uri, String selectionClause, Context context)
+    {
+        // References -https://developer.android.com/reference/android/database/MatrixCursor - Used to learn the concept of matrixCursor. It offers a function addRow(object[] columns) to add the row.
+        try {
+            // Creating an object of MatrixCursor which is basically the implementation of the cursor we can use
+            MatrixCursor matrixCursor = new MatrixCursor(new String[]{"key", "value"});
+            // Creating a stream object. Various other alternate byte/character streams can be used to read and write the data.
+            FileInputStream inputstream = context.openFileInput(selectionClause);
+            int res =0;
+            // creating an object of StringBuilder to efficiently append the data which is read using read() function. Read() function returns a byte and hence we use while loop to read all the bytes. It returns -1 if its empty.
+            StringBuilder sb = new StringBuilder();
+            while((res = inputstream.read())!= -1)
+            {
+                sb.append((char) res );
+
+            }
+            matrixCursor.addRow( new String[]{selectionClause, sb.toString()} );
+            inputstream.close();
+            return matrixCursor;
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
