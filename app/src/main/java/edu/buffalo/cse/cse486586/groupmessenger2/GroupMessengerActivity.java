2fff5e6222bb37225e0128359067e37d7771ab93
a_m_a_n <amanbhayana@yahoo.in>
2019-03-15 00:21:52 -0400

    Checkpoint

--- app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerActivity.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerActivity.java
@@ -19,11 +19,15 @@ import java.io.DataOutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
@@ -68,6 +72,7 @@ public class GroupMessengerActivity extends Activity {
     static double currentMaxProposedNo=0.00000;
     SortedMap<Double, String > messageToDeliverList = new TreeMap<Double, String>();
     Queue<Message> deliveryQueue = new LinkedList<Message>();
+    List<Message> deliveryMessageList  = new ArrayList<Message>();
     static int setCount =0;
     PriorityBlockingQueue<Message> holdBackQueue = new PriorityBlockingQueue<Message>(1000, new MessageCompare());
 
@@ -137,103 +142,89 @@ public class GroupMessengerActivity extends Activity {
     private class ServerTask extends AsyncTask<ServerSocket, String,Void>{
         @Override
         protected Void doInBackground(ServerSocket... serverSockets) {
-            ReentrantLock lock = new ReentrantLock();
+
 
             try {
                 TelephonyManager tel = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
                 String processID = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
-                Log.i("SERVER- " +Integer.valueOf(processID)*2, " My PROCESS ID-" + processID);
-                Log.i("SERVER-"+Integer.valueOf(processID)*2, " My PORT NO-" + Integer.valueOf(processID)*2);
+                Log.i("SERVER- " + Integer.valueOf(processID) * 2, " My PROCESS ID-" + processID);
+                Log.i("SERVER-" + Integer.valueOf(processID) * 2, " My PORT NO-" + Integer.valueOf(processID) * 2);
 
                 ServerSocket serverSocket = serverSockets[0];
-                while(true)
-                {
+                while (true) {
                     Socket socket = serverSocket.accept();
                     DataInputStream inputStream = new DataInputStream(socket.getInputStream());
                     String messageFromClient = inputStream.readUTF();
-                    Log.i("SERVER-"+Integer.valueOf(processID)*2, "MESSAGE RECEIVED FROM CLIENT" + messageFromClient);
+                    Log.i("SERVER-" + Integer.valueOf(processID) * 2, "MESSAGE RECEIVED FROM CLIENT" + messageFromClient);
 
-                    String [] messageFromClientTokens = messageFromClient.split(":");
-                    Log.i("Message length--",String.valueOf(messageFromClientTokens.length));
+                    String[] messageFromClientTokens = messageFromClient.split(":");
+                    Log.i("Message length--", String.valueOf(messageFromClientTokens.length));
 
-                    if( messageFromClientTokens.length == 3) {
-
-                        ManipulateClientMessage(messageFromClientTokens, socket, processID);
-                    }
+                    if (messageFromClientTokens.length == 3) ManipulateClientMessage(messageFromClientTokens, socket, processID);
                     else {
 
-                        Log.i("SERVER-" + Integer.valueOf(processID) * 2, "MESSAGE TO BE DELIVERED--"+ messageFromClientTokens[0] + " -" + messageFromClientTokens[1]);
+                        Log.i("SERVER-" + Integer.valueOf(processID) * 2, "MESSAGE TO BE DELIVERED--" + messageFromClientTokens[0] + " -" + messageFromClientTokens[1]);
                         Log.i("SERVER-" + Integer.valueOf(processID) * 2, "MESSAGE SENT");
 
-                            Iterator value = holdBackQueue.iterator();
-                            while (value.hasNext()) {
+                        Iterator value = holdBackQueue.iterator();
+                        while (value.hasNext()) {
 
-                                Message msg = (Message) value.next();
-                                if (msg.messageText.equals(messageFromClientTokens[1])) {
-                                    holdBackQueue.remove(msg);
-                                    msg.mySequenceNo = Double.valueOf(messageFromClientTokens[0]);
-                                    msg.toBeDelivered = true;
-                                    holdBackQueue.add(msg);
-                                }
+                            Message msg = (Message) value.next();
+                            if (msg.messageText.equals(messageFromClientTokens[1])) {
+                                holdBackQueue.remove(msg);
+                                msg.mySequenceNo = Double.valueOf(messageFromClientTokens[0]);
+                                msg.toBeDelivered = true;
 
+                                holdBackQueue.add(msg);
                             }
 
-                        //messageToDeliverList.put(Double.valueOf(messageFromClientTokens[2]),messageFromClientTokens[1]);
-                        //setCount++;
-
-                       // Set set = messageToDeliverList.entrySet();
-                        //Iterator iterator = set.iterator();
-
-                        if (holdBackQueue.peek() != null && holdBackQueue.peek().toBeDelivered == true) {
-                            deliveryQueue.add(holdBackQueue.poll());
-                        }
-                            myLargestAgreedSeqNo = Double.valueOf(messageFromClientTokens[2]);
-//
-//                            while (iterator.hasNext()) {
-//
-                                ContentValues content = new ContentValues();
-//                                Map.Entry m = (Map.Entry) iterator.next();
-//                                double key = (Double) m.getKey();
-//                                String message = (String) m.getValue();
-//
-//                                Log.i("Delivered Message List" +  Integer.valueOf(processID) * 2, "key-" + key + " " +  "Message-"+message );
-                               lock.lock();
-                                while(! deliveryQueue.isEmpty() && deliveryQueue.peek().toBeDelivered == true) {
-
-                                    Message message = deliveryQueue.poll();
-                                       Log.i("Final----", String.valueOf(actualSequenceNo) + "-" + message.messageText);
-                                        content.put("key", String.valueOf(actualSequenceNo++));
-                                        content.put("value", message.messageText);
-                                        getContentResolver().insert(CONTENT_URI, content);
-                                        publishProgress(actualSequenceNo + " -" + message.messageText);
-                                        lock.unlock();
-                                        Log.i("SERVER-" + Integer.valueOf(processID) * 2, "My largest agreed sequence no -" + myLargestAgreedSeqNo);
-                                        //messageToDeliverList.remove(key);
-
-
-                                }
-                                }
-
-
                         }
 
-                        //for (Map.Entry<Integer, String> entry : map.entrySet()) {
-                       //     System.out.println(entry.getKey() + " => " + entry.getValue());
-                       // }
+
+                        while (holdBackQueue.peek() != null && holdBackQueue.peek().toBeDelivered == true) {
+                            Message toDeliverMessage = holdBackQueue.poll();
+                            toDeliverMessage.mySequenceNo = toDeliverMessage.mySequenceNo > myLargestAgreedSeqNo ? toDeliverMessage.mySequenceNo : myLargestAgreedSeqNo;
+                            deliveryMessageList.add(toDeliverMessage);
+                            deliveryQueue.add(toDeliverMessage);
+                            setCount++;
+                        }
+                        myLargestAgreedSeqNo = Double.valueOf(messageFromClientTokens[2]);
+                        ContentValues content = new ContentValues();
+
+                        //while (!deliveryQueue.isEmpty() && deliveryQueue.peek().toBeDelivered == true) {
+                        for(int i =0 ; i< deliveryMessageList.size();i ++)
+                        {
+                            Log.i("Item " + i , deliveryMessageList.get(i).messageText + "--" + deliveryMessageList.get(i).mySequenceNo );
+                        }
+                        Collections.sort(deliveryMessageList,new MessageCompare());
+                        Log.i("List", " After sort");
+                        for(int i =0 ; i< deliveryMessageList.size();i ++)
+                        {
+                            Log.i("Item " + i , deliveryMessageList.get(i).messageText + "--" + deliveryMessageList.get(i).mySequenceNo );
+                        }
+                        for(int i =0; i< deliveryMessageList.size();i++)
+                        {
+                         //   Message message = deliveryQueue.poll();
+                          //  if (message.mySequenceNo > deliveryQueue.peek().mySequenceNo) {
+                            Message message = deliveryMessageList.remove(i);
+                                Log.i("Final----", String.valueOf(actualSequenceNo) + "-" + message.messageText);
+                                content.put("key", String.valueOf(actualSequenceNo++));
+                                content.put("value", message.messageText);
+                                getContentResolver().insert(CONTENT_URI, content);
+                                publishProgress(actualSequenceNo + " -" + message.messageText);
+                                Log.i("SERVER-" + Integer.valueOf(processID) * 2, "My largest agreed sequence no -" + myLargestAgreedSeqNo);
+                            }
+                        }
+                    }
 
 
-                   // }
+                }
+            catch(Exception ex){
+                    ex.printStackTrace();
+                }
+                return null;
 
-
-
-
-            }
-            catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            return null;
         }
-
 
         protected void onProgressUpdate(String... values) {
             String strReceived = values[0].trim();
@@ -255,11 +246,11 @@ public class GroupMessengerActivity extends Activity {
 
             holdBackQueue.add(new Message(myProposedSeqNo,messageFromClientTokens[1],false));
             Log.i("SERVER-"+Integer.valueOf(processID)*2,"Holdback Queue Data at head 1st time- " +  holdBackQueue.peek().messageText);
-            Log.i("SERVER-"+Integer.valueOf(processID)*2, " My proposed sequence no-" +myProposedSeqNo);
-            Log.i("SERVER-"+Integer.valueOf(processID)*2, "MESSAGE SENDING FROM SERVER" + Integer.valueOf(processID) * 2 + ":" + messageFromClientTokens[1] + ":" + myProposedSeqNo);
+           // Log.i("SERVER-"+Integer.valueOf(processID)*2, " My proposed sequence no-" +myProposedSeqNo);
+            //Log.i("SERVER-"+Integer.valueOf(processID)*2, "MESSAGE SENDING FROM SERVER" + Integer.valueOf(processID) * 2 + ":" + messageFromClientTokens[1] + ":" + myProposedSeqNo);
             outputStream.writeUTF(  Integer.valueOf(processID) * 2 + ":" + messageFromClientTokens[1] + ":" + myProposedSeqNo);
             outputStream.flush();
-            Log.i("SERVER-"+Integer.valueOf(processID)*2,"MESSAGE SENT");
+            //Log.i("SERVER-"+Integer.valueOf(processID)*2,"MESSAGE SENT");
             currentMaxProposedNo = myProposedSeqNo;
         }catch(Exception ex)
         {
